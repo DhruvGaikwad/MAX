c2a66cc65036a5bd4c64710b485174390b8f3c99
DhruvGaikwad <dhruvgaikwad4@gmail.com>
2025-10-07 23:48:15 +0530

    added googlesearch (does not work yet)

--- MAX/main.py
+++ MAX/main.py
@@ -1,20 +1,30 @@
 import speech_recognition as sr
-
+from googlesearch import search
+import webbrowser as wb
+import time 
 # Initialize recognizer
 r = sr.Recognizer()
 
 # Use microphone as source
 with sr.Microphone() as source: 
     print("Say something...")
+    r.adjust_for_ambient_noise(source)
 
     while True:
-        r.adjust_for_ambient_noise(source)
+        
         print("Listening...")
         audio = r.listen(source)
 
         try:
             text = r.recognize_google(audio)
             print("You said:", text)
+
+            query = text
+            for url in search(query):
+                print("Opening:", url)
+                wb.open(url)
+                time.sleep(2)
+                
             
         except sr.UnknownValueError:
             print("Sorry, I could not understand the audio.")
